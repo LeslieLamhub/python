667af1cb8a629bc4a86a0f41158ec67952948c39
Leslie <http.me.com@me.com>
2019-01-04 11:04:08 +0800

    xe-vm updata

--- Exercise/xe-vm.py
+++ Exercise/xe-vm.py
@@ -5,4 +5,4 @@ import sys
 if os.system('xe vm-export vm=platform-bigdata-03-8.114 filename=/var/run/sr-mount/7c470c77-76b7-10ee-5998-0388a6834936/platform-bigdata-03-8.114.xva') ==0:
     os.system('xe vm-export vm=platform-bigdata-04-8.115 filename=/var/run/sr-mount/7c470c77-76b7-10ee-5998-0388a6834936/platform-bigdata-04-8.115.xva')
 else:
-    print 'This is a bad command'
+    print ('This is a bad command')
